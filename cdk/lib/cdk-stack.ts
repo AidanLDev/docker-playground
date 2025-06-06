0d51734a4a444b4824cea6423777a7c3525d3b29
Aidan Lowson <dev@aidanlowson.com>
2025-06-06 23:44:42 +0100

    Updating config to open ports up

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -7,14 +7,38 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new ecs.Cluster(this, "DockerPlaygroundCluster", {
+    const cluster = new ecs.Cluster(this, "DockerPlaygroundCluster", {
       clusterName: "docker-playground-cluster",
     });
 
-    new ecr.Repository(this, "DockerPlaygroundRepo", {
+    const repo = new ecr.Repository(this, "DockerPlaygroundRepo", {
       repositoryName: "docker-playground-repo",
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       emptyOnDelete: true,
+    });
+
+    const taskDef = new ecs.FargateTaskDefinition(
+      this,
+      "DockerPlaygroundTaskDef",
+      {
+        memoryLimitMiB: 512,
+        cpu: 256,
+      }
+    );
+
+    // Add container to task definition
+    taskDef.addContainer("DockerPlaygroundContainer", {
+      image: ecs.ContainerImage.fromEcrRepository(repo),
+      portMappings: [{ containerPort: 3000 }],
+      essential: true,
+    });
+
+    // Create Fargate Service
+    new ecs.FargateService(this, "DockerPlaygroundService", {
+      cluster: cluster,
+      taskDefinition: taskDef,
+      desiredCount: 1,
+      assignPublicIp: true,
     });
   }
 }
